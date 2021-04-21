f4be57b5c26d04e38abc818c5c4866254f6d91e7
xwyan1230 <xwyan1230@gmail.com>
2021-04-21 12:21:29 -0700

    correct the counting for files to avoid no title situation

--- analysis/frap_03_file-processing_multi-well.py
+++ analysis/frap_03_file-processing_multi-well.py
@@ -1,10 +1,8 @@
 import os
 import shared.dataframe as dat
 
-multi_data_source = "/Users/xiaoweiyan/Dropbox/LAB/ValeLab/Projects/Blob_bleacher/Data/"\
-    "20210408_CBB_nucleoliFRAPscreen1/dataAnalysis/"
-save_source = "/Users/xiaoweiyan/Dropbox/LAB/ValeLab/Projects/Blob_bleacher/Data/"\
-    "20210408_CBB_nucleoliFRAPscreen1/dataFiles/"
+multi_data_source = "D:/Xiaowei/data/20210415_CBB_nucleoliFRAPscreen2/plate2/dataAnalysis/"
+save_source = "D:/Xiaowei/data/20210415_CBB_nucleoliFRAPscreen2/plate2/dataFiles/"
 
 analyze_organelle = 'nucleoli'  # only accepts 'sg' or 'nucleoli'
 
@@ -32,29 +30,31 @@ for r in range(len(multi_dirs)):
     elif analyze_organelle == 'sg':
         f_sg = open("%s/%s_data_sg.txt" % (save_path, name), 'w+')
 
+    count = 0
     for s in range(len(dirs)):
         data_path = dirs[s]
         if os.path.exists("%s/data_full.txt" % data_path):
             f1_full = open("%s/data_full.txt" % data_path, 'r')
-            dat.append_data(f_full, f1_full, s)
+            dat.append_data(f_full, f1_full, count)
             f1_full.close()
             f1_log = open("%s/data_log.txt" % data_path, 'r')
-            dat.append_data(f_log, f1_log, s)
+            dat.append_data(f_log, f1_log, count)
             f1_log.close()
             f1_ctrl = open("%s/data_ctrl.txt" % data_path, 'r')
-            dat.append_data(f_ctrl, f1_ctrl, s)
+            dat.append_data(f_ctrl, f1_ctrl, count)
             f1_ctrl.close()
             if analyze_organelle == 'nucleoli':
                 f1_nuclear = open("%s/data_nuclear.txt" % data_path, 'r')
-                dat.append_data(f_nuclear, f1_nuclear, s)
+                dat.append_data(f_nuclear, f1_nuclear, count)
                 f1_nuclear.close()
                 f1_nucleoli = open("%s/data_nucleoli.txt" % data_path, 'r')
-                dat.append_data(f_nucleoli, f1_nucleoli, s)
+                dat.append_data(f_nucleoli, f1_nucleoli, count)
                 f1_nucleoli.close()
             elif analyze_organelle == 'sg':
                 f1_sg = open("%s/data_sg.txt" % data_path, 'r')
-                dat.append_data(f_sg, f1_sg, s)
+                dat.append_data(f_sg, f1_sg, count)
                 f1_sg.close()
+            count = count + 1
 
     f_full.close()
     f_log.close()
