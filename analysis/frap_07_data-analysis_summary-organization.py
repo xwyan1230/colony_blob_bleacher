55b5f307fad74b119a7698a301db229fe7a892d9
Xiaowei Yan <xiaoweiyan@Xiaoweis-MacBook-Pro.local.ucsf.edu>
2021-04-21 13:37:12 -0700

    for multiple plate summary organization

---
+++ analysis/frap_07_data-analysis_summary-organization.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import os
+
+multi_data_source = "/Users/xiaoweiyan/Dropbox/LAB/ValeLab/Projects/Blob_bleacher/Exp/20210415_CBB_nucleoliFRAPscreen2/"
+save_source = "/Users/xiaoweiyan/Dropbox/LAB/ValeLab/Projects/Blob_bleacher/Exp/"\
+    "20210415_CBB_nucleoliFRAPscreen2_summary/summary/"
+WT = 'NT'
+
+multi_dirs = [x for x in os.listdir(multi_data_source)]
+if '.DS_Store' in multi_dirs:
+    multi_dirs.remove('.DS_Store')
+
+data_p = pd.DataFrame()
+data_value = pd.DataFrame()
+
+for r in range(len(multi_dirs)):
+    name = multi_dirs[r]
+    number = name[5:]
+    print('# Organizing %s ... (%d/%d)' % (name, r+1, len(multi_dirs)))
+    data_p_source = ("%s%s/dataSummary/%s/summary/" % (multi_data_source, name, WT))
+    data_value_source = ("%s%s/dataPlots/%s/summary/" % (multi_data_source, name, WT))
+    save_path = ("%s%s/" % (save_source, WT))
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    data_p_temp = pd.read_csv(("%ssummary_p.txt" % data_p_source), na_values=['.'], sep='\t')
+    data_p_temp['plate'] = [number] * len(data_p_temp)
+    data_value_temp = pd.read_csv(("%ssummary_value.txt" % data_value_source), na_values=['.'], sep='\t')
+    data_value_temp['plate'] = [number] * len(data_value_temp)
+
+    data_p = pd.concat([data_p, data_p_temp], ignore_index=True)
+    data_value = pd.concat([data_value, data_value_temp], ignore_index=True)
+
+data_p.to_csv('%ssummary_p.txt' % save_path, index=False, sep='\t')
+data_value.to_csv('%ssummary_value.txt' % save_path, index=False, sep='\t')
+
+print("DONE!")
