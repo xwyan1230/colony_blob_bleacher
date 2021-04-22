143659cce4afcfd39cfd74ff45199462511b883f
Xiaowei Yan <xiaoweiyan@Xiaoweis-MacBook-Pro.local>
2021-04-22 16:32:35 -0700

    simplified script

--- analysis/frap_08_data-analysis_volcano-sum.py
+++ analysis/frap_08_data-analysis_volcano-sum.py
@@ -3,26 +3,22 @@ import shared.display as dis
 import os
 
 data_source = "/Users/xiaoweiyan/Dropbox/LAB/ValeLab/Projects/Blob_bleacher/Exp/"\
-    "20210415_CBB_nucleoliFRAPscreen2/plate2/summary/NT/"
+    "20210408_CBB_nucleoliFRAPscreen1/plate1/summary/WT/"
 save_source = "/Users/xiaoweiyan/Dropbox/LAB/ValeLab/Projects/Blob_bleacher/Exp/"\
-    "20210415_CBB_nucleoliFRAPscreen2/plate2/volcanoPlots/NT/"
+    "20210408_CBB_nucleoliFRAPscreen1/plate1/volcanoPlots/WT/"
+
+features = ['mob', 'curve_mob', 't_half', 'curve_t_half', 'slope', 'curve_slope', 'size_organelle', 'raw_int_organelle',
+            'circ_organelle', 'ecce_organelle']
 
 data_p = pd.read_csv(("%ssummary_p.txt" % data_source), na_values=['.'], sep='\t')
 data_value = pd.read_csv(("%ssummary_value.txt" % data_source), na_values=['.'], sep='\t')
+data_gamma = pd.read_csv(("%ssummary_gamma.txt" % data_source), na_values=['.'], sep='\t')
 
 if not os.path.exists(save_source):
     os.makedirs(save_source)
 
 # export volcano plots
-dis.plot_volcano(data_p, data_value, 'mob', save_source)
-dis.plot_volcano(data_p, data_value, 'curve_mob', save_source)
-dis.plot_volcano(data_p, data_value, 't_half', save_source)
-dis.plot_volcano(data_p, data_value, 'curve_t_half', save_source)
-dis.plot_volcano(data_p, data_value, 'slope', save_source)
-dis.plot_volcano(data_p, data_value, 'curve_slope', save_source)
-dis.plot_volcano(data_p, data_value, 'size_organelle', save_source)
-dis.plot_volcano(data_p, data_value, 'raw_int_organelle', save_source)
-dis.plot_volcano(data_p, data_value, 'circ_organelle', save_source)
-dis.plot_volcano(data_p, data_value, 'ecce_organelle', save_source)
+for i in range(len(features)):
+    dis.plot_volcano(data_p, data_value, features[i], save_source)
 
 print("DONE!")
