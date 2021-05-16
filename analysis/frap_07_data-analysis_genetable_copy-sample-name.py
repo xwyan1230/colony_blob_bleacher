3a09df8db8618792ae8cba59dd330e2c764b20d8
xwyan1230 <xwyan1230@gmail.com>
2021-05-16 15:55:09 -0700

    add same sample name as gene name for script running without further genetable

---
+++ analysis/frap_07_data-analysis_genetable_copy-sample-name.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+data_source = "D:/Xiaowei/data/20210510_ctrlComparison/"
+WT = 'WT'
+
+data_p_source = ("%sdataSummary/%s/summary/" % (data_source, WT))
+data_value_source = ("%sdataPlots/%s/summary/" % (data_source, WT))
+data_p = pd.read_csv(("%ssummary_p.txt" % data_p_source), na_values=['.'], sep='\t')
+data_value = pd.read_csv(("%ssummary_value.txt" % data_value_source), na_values=['.'], sep='\t')
+save_path = ("%ssummary/%s/" % (data_source, WT))
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+
+data_p['gene'] = data_p['sample']
+data_value['gene'] = data_value['sample']
+
+data_p.to_csv('%ssummary_p.txt' % save_path, index=False, sep='\t')
+data_value.to_csv('%ssummary_value.txt' % save_path, index=False, sep='\t')
+
+print("DONE!")
