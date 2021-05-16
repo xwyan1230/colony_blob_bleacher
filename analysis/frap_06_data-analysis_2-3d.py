eda0a882fc388e3d356c9498955181f570bbf316
xwyan1230 <xwyan1230@gmail.com>
2021-05-16 15:54:32 -0700

    add feature to not display images to save time

--- analysis/frap_06_data-analysis_2-3d.py
+++ analysis/frap_06_data-analysis_2-3d.py
@@ -3,12 +3,13 @@ import pandas as pd
 import shared.display as dis
 import numpy as np
 
-multi_data_source = "D:/Xiaowei/data/20210415_CBB_nucleoliFRAPscreen2/plate1/dataFiles/"
-WT_source = "D:/Xiaowei/data/20210415_CBB_nucleoliFRAPscreen2/plate1/WTFiles/NT_G2-G3/"
-save_source = "D:/Xiaowei/data/20210415_CBB_nucleoliFRAPscreen2/plate1/dataPlots/NT_G2-G3/"
+multi_data_source = "D:/Xiaowei/data/20210510_ctrlComparison/dataFiles/"
+WT_source = "D:/Xiaowei/data/20210510/WTFiles/WT_B2-B3-B4/"
+save_source = "D:/Xiaowei/data/20210510_ctrlComparison/dataPlots/WT_P1-P2_P3/"
 
 analyze_organelle = 'nucleoli'  # only accepts 'sg' or 'nucleoli'
 analysis_mode = 'single_exp'
+export_figure_mode = 'off'
 
 bounds_mob = np.array([0, 0.4, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.9, 1, 1.2])
 bounds_t_half = np.array([0, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 3.0])
@@ -39,8 +40,6 @@ for r in range(len(multi_dirs)):
     print('# Analyzing %s ... (%d/%d)' % (name, r+1, len(multi_dirs)))
     data_source = ("%s%s/" % (multi_data_source, name))
     save_path = ("%s%s/" % (save_source, name))
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
 
     data_sample = pd.read_csv(("%s%s_data_full.txt" % (data_source, name)), na_values=['.'], sep='\t')
     data_sample_ft = data_sample[data_sample['frap_filter_%s' % analysis_mode] == 1]
@@ -57,26 +56,9 @@ for r in range(len(multi_dirs)):
     slope.append(np.mean(data_sample_ft['%s_slope' % analysis_mode]))
     curve_slope.append(np.mean(data_sample_ft['linear_slope']))
 
-    # export FRAP 3d images
-    print("# Export mobile_fraction 3d plot ...")
-    dis.plot_frap_3d(data_WT_ft, data_sample_ft, 'mobile_fraction', bounds_mob, name, 'curve_mob', save_path)
-    dis.plot_frap_3d(data_WT_ft, data_sample_ft, '%s_mobile_fraction' % analysis_mode, bounds_mob, name, 'mob',
-                     save_path)
-    print("# Export t_half 3d plot ...")
-    dis.plot_frap_3d(data_WT_ft, data_sample_ft, 't_half', bounds_t_half, name, 'curve_t_half', save_path)
-    dis.plot_frap_3d(data_WT_ft, data_sample_ft, '%s_t_half' % analysis_mode, bounds_t_half, name, 't_half',
-                     save_path)
-    print("# Export slope 3d plot ...")
-    dis.plot_frap_3d(data_WT_ft, data_sample_ft, 'linear_slope', bounds_slope, name, 'curve_slope', save_path)
-    dis.plot_frap_3d(data_WT_ft, data_sample_ft, '%s_slope' % analysis_mode, bounds_slope, name, 'slope',
-                     save_path)
-
-    if analyze_organelle == 'nucleoli':
-        data_sample_organelle = pd.read_csv(("%s%s_data_nucleoli.txt" % (data_source, name)), na_values=['.'], sep='\t')
-        data_WT_organelle = pd.read_csv(("%sWT_data_nucleoli.txt" % WT_source), na_values=['.'], sep='\t')
-    elif analyze_organelle == 'sg':
-        data_sample_organelle = pd.read_csv(("%s%s_data_sg.txt" % (data_source, name)), na_values=['.'], sep='\t')
-        data_WT_organelle = pd.read_csv(("%sWT_data_sg.txt" % WT_source), na_values=['.'], sep='\t')
+    data_sample_organelle = pd.read_csv(("%s%s_data_%s.txt" % (data_source, name, analyze_organelle)), na_values=['.'],
+                                        sep='\t')
+    data_WT_organelle = pd.read_csv(("%sWT_data_%s.txt" % (WT_source, analyze_organelle)), na_values=['.'], sep='\t')
 
     data_organelle_n.append(len(data_sample_organelle))
     data_organelle_n_circ.append(len(data_sample_organelle[data_sample_organelle['size'] > 50]))
@@ -87,20 +69,38 @@ for r in range(len(multi_dirs)):
     circ.append(np.mean(data_sample_organelle[data_sample_organelle['size'] > 50]['circ']))
     ecce.append(np.mean(data_sample_organelle['eccentricity']))
 
-    # export organelle 2d images
-    print("# Export organelle 2d images ...")
-    dis.plot_organelle_2d(data_WT_organelle, data_sample_organelle, 'raw_int', 'log', 'ln(raw intensity)',
-                          'size', 'linear', 'size (pixel)', name, save_path)
-    dis.plot_organelle_2d(data_WT_organelle, data_sample_organelle, 'raw_int', 'log', 'ln(raw intensity)',
-                          'circ', 'linear', 'circularity', name, save_path)
-    dis.plot_organelle_2d(data_WT_organelle, data_sample_organelle, 'raw_int', 'log', 'ln(raw intensity)',
-                          'eccentricity', 'linear', 'eccentricity', name, save_path)
-    dis.plot_organelle_2d(data_WT_organelle, data_sample_organelle, 'size', 'linear', 'size (pixel)',
-                          'eccentricity', 'linear', 'eccentricity', name, save_path)
-    dis.plot_organelle_2d(data_WT_organelle, data_sample_organelle, 'circ', 'linear', 'circularity',
-                          'eccentricity', 'linear', 'eccentricity', name, save_path)
-    dis.plot_organelle_2d(data_WT_organelle, data_sample_organelle, 'size', 'linear', 'size (pixel)',
-                          'circ', 'linear', 'circularity', name, save_path)
+    if export_figure_mode == 'on':
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
+        # export FRAP 3d images
+        print("# Export mobile_fraction 3d plot ...")
+        dis.plot_frap_3d(data_WT_ft, data_sample_ft, 'mobile_fraction', bounds_mob, name, 'curve_mob', save_path)
+        dis.plot_frap_3d(data_WT_ft, data_sample_ft, '%s_mobile_fraction' % analysis_mode, bounds_mob, name, 'mob',
+                         save_path)
+        print("# Export t_half 3d plot ...")
+        dis.plot_frap_3d(data_WT_ft, data_sample_ft, 't_half', bounds_t_half, name, 'curve_t_half', save_path)
+        dis.plot_frap_3d(data_WT_ft, data_sample_ft, '%s_t_half' % analysis_mode, bounds_t_half, name, 't_half',
+                         save_path)
+        print("# Export slope 3d plot ...")
+        dis.plot_frap_3d(data_WT_ft, data_sample_ft, 'linear_slope', bounds_slope, name, 'curve_slope', save_path)
+        dis.plot_frap_3d(data_WT_ft, data_sample_ft, '%s_slope' % analysis_mode, bounds_slope, name, 'slope',
+                         save_path)
+
+        # export organelle 2d images
+        print("# Export organelle 2d images ...")
+        dis.plot_organelle_2d(data_WT_organelle, data_sample_organelle, 'raw_int', 'log', 'ln(raw intensity)',
+                              'size', 'linear', 'size (pixel)', name, save_path)
+        dis.plot_organelle_2d(data_WT_organelle, data_sample_organelle, 'raw_int', 'log', 'ln(raw intensity)',
+                              'circ', 'linear', 'circularity', name, save_path)
+        dis.plot_organelle_2d(data_WT_organelle, data_sample_organelle, 'raw_int', 'log', 'ln(raw intensity)',
+                              'eccentricity', 'linear', 'eccentricity', name, save_path)
+        dis.plot_organelle_2d(data_WT_organelle, data_sample_organelle, 'size', 'linear', 'size (pixel)',
+                              'eccentricity', 'linear', 'eccentricity', name, save_path)
+        dis.plot_organelle_2d(data_WT_organelle, data_sample_organelle, 'circ', 'linear', 'circularity',
+                              'eccentricity', 'linear', 'eccentricity', name, save_path)
+        dis.plot_organelle_2d(data_WT_organelle, data_sample_organelle, 'size', 'linear', 'size (pixel)',
+                              'circ', 'linear', 'circularity', name, save_path)
 
 data_frap = pd.DataFrame({'sample': data_name, 'n_curve': data_frap_n_curve, 'mob': mob, 'curve_mob': curve_mob,
                           't_half': t_half, 'curve_t_half': curve_t_half, 'slope': slope, 'curve_slope': curve_slope,
@@ -114,8 +114,3 @@ if not os.path.exists(save_path):
 data_frap.to_csv('%ssummary_value.txt' % save_path, index=False, sep='\t')
 
 print("DONE!")
-
-
-
-
-
