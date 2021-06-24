74efa428d80cc0c3e95fb59c9c96454f80c5680c
Xiaowei Yan <xiaoweiyan@Xiaoweis-MacBook-Pro.local.ucsf.edu>
2021-06-24 10:17:41 -0700

    add into masterlog function

--- analysis/frap_01_screen_folder-organization.py
+++ analysis/frap_01_screen_folder-organization.py
@@ -1,8 +1,27 @@
 import os
 import shutil
+from datetime import datetime
 
+# input parameters
+master_folder = "D:/Xiaowei/data/20210607_screen/"
 data_source = "D:/Xiaowei/data/20210607_screen/"
 
+# log all the running info
+if not os.path.exists("%sscreen_processing_log.txt" % master_folder):
+    f = open("%sscreen_processing_log.txt" % master_folder, "w+")
+else:
+    f = open("%sscreen_processing_log.txt" % master_folder, "a+")
+
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+f.write("datetime: %s\n" % dt_string)
+f.write("code_running: %s\n" % __file__)
+f.write("master_folder: %s\n" % master_folder)
+f.write("data_source: %s\n\n" % data_source)
+
+f.close()
+
+# script start
 dirs = [x[0] for x in os.walk(data_source)]
 dirs.pop(0)
 num_dir = len(dirs)
