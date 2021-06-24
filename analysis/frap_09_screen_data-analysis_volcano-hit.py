8a7cf6590f2de59440c9665c437db7fb1ac1907a
Xiaowei Yan <xiaoweiyan@Xiaoweis-MacBook-Pro.local.ucsf.edu>
2021-06-24 10:19:21 -0700

    add into masterlog function

--- analysis/frap_09_screen_data-analysis_volcano-hit.py
+++ analysis/frap_09_screen_data-analysis_volcano-hit.py
@@ -2,7 +2,9 @@ import pandas as pd
 import shared.display as dis
 import numpy as np
 import os
+from datetime import datetime
 
+master_folder = "D:/Xiaowei/data/20210607_screen/"
 data_source = "D:/Xiaowei/data/20210607_screen/summary/NT/"
 save_source = "D:/Xiaowei/data/20210607_screen/volcanoPlots/NT/"
 WT_source = "D:/Xiaowei/data/20210607_screen/WTFiles/NT/"
@@ -11,6 +13,27 @@ analyze_organelle = 'nucleoli'
 analysis_mode = 'single_exp'
 features = ['mob', 'curve_mob', 't_half', 'curve_t_half', 'slope', 'curve_slope', 'size_organelle', 'raw_int_organelle',
             'circ_organelle', 'ecce_organelle']
+
+# log all the running info
+if not os.path.exists("%sscreen_processing_log.txt" % master_folder):
+    f = open("%sscreen_processing_log.txt" % master_folder, "w+")
+else:
+    f = open("%sscreen_processing_log.txt" % master_folder, "a+")
+
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+f.write("datetime: %s\n" % dt_string)
+f.write("code_running: %s\n" % __file__)
+f.write("master_folder: %s\n" % master_folder)
+f.write("data_source: %s\n" % data_source)
+f.write("save_source: %s\n" % save_source)
+f.write("WT_source: %s\n" % WT_source)
+f.write("analyze_organelle: %s\n" % analyze_organelle)
+f.write("analysis_mode: %s\n" % analysis_mode)
+f.write("features: %s\n\n" % features)
+
+f.close()
+
 
 data_WT = pd.read_csv(("%sWT_data_full.txt" % WT_source), na_values=['.'], sep='\t')
 data_WT_ft = data_WT[data_WT['frap_filter_%s' % analysis_mode] == 1]
