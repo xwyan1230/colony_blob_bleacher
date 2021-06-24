c692c234ea43bc5c7e7edc40176d53cc6ccadeba
xwyan1230 <xwyan1230@gmail.com>
2021-06-23 23:53:10 -0700

    add into stdev file

--- analysis/frap_07_screen_data-analysis_genetable_single-plate.py
+++ analysis/frap_07_screen_data-analysis_genetable_single-plate.py
@@ -7,8 +7,10 @@ WT = 'NT'
 
 data_p_source = ("%sdataSummary/%s/summary/" % (data_source, WT))
 data_value_source = ("%sdataPlots/%s/summary/" % (data_source, WT))
+data_stdev_source = ("%sdataStdev/%s/summary/" % (data_source, WT))
 data_p = pd.read_csv(("%ssummary_p.txt" % data_p_source), na_values=['.'], sep='\t')
 data_value = pd.read_csv(("%ssummary_value.txt" % data_value_source), na_values=['.'], sep='\t')
+data_stdev = pd.read_csv(("%ssummary_stdev.txt" % data_stdev_source), na_values=['.'], sep='\t')
 genetable = pd.read_csv(("%sgenetable.txt" % data_source), na_values=['.'], sep='\t')
 save_path = ("%ssummary/%s/" % (data_source, WT))
 if not os.path.exists(save_path):
@@ -16,8 +18,10 @@ if not os.path.exists(save_path):
 
 data_p = dat.correlate_genetable(data_p, genetable)
 data_value = dat.correlate_genetable(data_value, genetable)
+data_stdev = dat.correlate_genetable(data_stdev, genetable)
 
 data_p.to_csv('%ssummary_p.txt' % save_path, index=False, sep='\t')
 data_value.to_csv('%ssummary_value.txt' % save_path, index=False, sep='\t')
+data_stdev.to_csv('%ssummary_stdev.txt' % save_path, index=False, sep='\t')
 
 print("DONE!")
